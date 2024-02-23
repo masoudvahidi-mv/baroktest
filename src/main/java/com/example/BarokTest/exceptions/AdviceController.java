65c722db650fa4b5219a0f0e50a1d7146ddfa0a7
hassanpoor <en.hassanpoor@gmail.com>
2024-02-23 11:51:43 +0330

    Update AdviceController.java

--- src/main/java/com/example/BarokTest/exceptions/AdviceController.java
+++ src/main/java/com/example/BarokTest/exceptions/AdviceController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+// az inteface ha ina estefade kon k badan exception handling barat rahat tar beshe 
 @ControllerAdvice
 public class AdviceController {
     @ExceptionHandler(value = UserNotFound.class)
