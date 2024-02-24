0aeb4e25e6fc73b282ea651271d3cf481f7850cb
Masoud Vahidi <95270624+masoudvahidi-mv@users.noreply.github.com>
2024-02-24 16:15:23 +0330

    Add files via upload

---
+++ src/main/java/com/example/BarokTest/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.BarokTest.repository;
+
+import com.example.BarokTest.entity.User;
+import com.example.BarokTest.genericDAO.GenericDao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long>, GenericDao<User,Long> {
+}
